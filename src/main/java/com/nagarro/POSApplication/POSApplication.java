ac29a771231de1110575cd6892770a0914da4b67
vineet7838 <vineetagarwal267@gmail.com>
2019-01-03 13:00:22 +0530

    farji

--- src/main/java/com/nagarro/POSApplication/POSApplication.java
+++ src/main/java/com/nagarro/POSApplication/POSApplication.java
@@ -1,5 +1,5 @@
 package com.nagarro.POSApplication;
-
+//sdgsf
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
